9375d4bfbfcb4ae0a4f2ca6068192dfd2d7c1e30
MgHtinLynn <htinlin01@gmail.com>
2022-10-30 02:17:38 +0700

    fix: for deployment with postgres

--- configs/connection.go
+++ configs/connection.go
@@ -13,24 +13,27 @@ func Connection() *gorm.DB {
 	viper.AutomaticEnv()
 	_ = viper.ReadInConfig()
 
-	viperHost := viper.Get("DB_HOST").(string)
+	//viperHost := viper.Get("DB_HOST").(string)
+	//
+	//viperPort := viper.Get("DB_PORT").(string)
 
-	viperPort := viper.Get("DB_PORT").(string)
+	// viperUser := viper.Get("DB_USERNAME").(string)
 
-	viperUser := viper.Get("DB_USERNAME").(string)
+	//viperDB := viper.Get("DB_NAME").(string)
+	//
+	//viperPassword := viper.Get("DB_PASSWORD").(string)
+	//
+	//viperSSLMode := viper.Get("DB_SSL_MODE").(string)
+	//
+	viperDBURL := viper.Get("DB_URL").(string)
 
-	viperDB := viper.Get("DB_NAME").(string)
+	fmt.Println("viperDBURL", viperDBURL)
 
-	viperPassword := viper.Get("DB_PASSWORD").(string)
+	//dbUrl := fmt.Sprintf("host=%v port=%v user=%v dbname=%v password=%v sslmode=%v", viperHost, viperPort, viperUser, viperDB, viperPassword, viperSSLMode)
 
-	viperSSLMode := viper.Get("DB_SSL_MODE").(string)
+	fmt.Println("dbUrl is\t\t", viperDBURL)
 
-	fmt.Println("viperUser", viperUser)
-
-	dbUrl := fmt.Sprintf("host=%v port=%v user=%v dbname=%v password=%v sslmode=%v", viperHost, viperPort, viperUser, viperDB, viperPassword, viperSSLMode)
-	fmt.Println("dbUrl is\t\t", dbUrl)
-
-	db, err := gorm.Open(postgres.Open(dbUrl), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(viperDBURL), &gorm.Config{})
 	//
 	if err != nil {
 		fmt.Println("err", err)
